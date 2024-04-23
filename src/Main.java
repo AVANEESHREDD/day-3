95a9a066d247de385691348a7fbf2bcaa99af8ab
DELL <tshiva0112@gmail.com>
2024-04-23 21:54:16 +0530

    conditions

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,39 @@
 
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int age= 20;
-        if(age>=18) {
-            System.out.println("Major");
+    //    int age= 20;
+      //  if(age>=18) {
+        //    System.out.println("Major");
+        //}else{
+          //  System.out.println("minor");
+//
+  //      }
+   // Scanner input=new Scanner(System.in);
+     //   System.out.println("PLease enter your age ");
+     //   int age=input.nextInt();
+        //if(age>=18){
+       //     System.out.println("Major");
+
+    //    }else{
+      //      System.out.println("Minor21");
+        //}
+    //else if
+        Scanner input=new Scanner(System.in);
+        System.out.println("PLease enter your marks ");
+        int score=input.nextInt();
+        if(score>=70){
+            System.out.println("Grade A");
+        }else if(score<70 && score  <=50) {
+            System.out.println("Garde B");
+        } else if(score<50 && score>=30) {
+            System.out.println("Grade B");
         }else{
-            System.out.println("minor");
+            System.out.println("Fail");
 
         }
+
     }
+
 }
